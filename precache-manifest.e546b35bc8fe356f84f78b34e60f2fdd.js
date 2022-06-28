1f3daa203a50fd4ae3a819662678815a55828a2e
MWERO <mwerothewebmaker@gmail.com>
2022-06-28 19:56:05 +0300

    Updates

---
+++ precache-manifest.e546b35bc8fe356f84f78b34e60f2fdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f52d68099364da031b49",
+    "url": "/react-todolist-app/static/css/main.6e099011.chunk.css"
+  },
+  {
+    "revision": "f52d68099364da031b49",
+    "url": "/react-todolist-app/static/js/main.f52d6809.chunk.js"
+  },
+  {
+    "revision": "17a735bb57d51e41b4b4",
+    "url": "/react-todolist-app/static/css/1.69bcce61.chunk.css"
+  },
+  {
+    "revision": "17a735bb57d51e41b4b4",
+    "url": "/react-todolist-app/static/js/1.17a735bb.chunk.js"
+  },
+  {
+    "revision": "6925251f480db203a770",
+    "url": "/react-todolist-app/static/js/runtime~main.6925251f.js"
+  },
+  {
+    "revision": "eba9b2de2adb8ae725eee5ddabe0cc3f",
+    "url": "/react-todolist-app/index.html"
+  }
+];
