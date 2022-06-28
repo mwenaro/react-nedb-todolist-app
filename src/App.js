18e444914cb4fd2499f445cc2c952cbaedc17a43
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-06-28 16:55:13 +0300

    Fixing a bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import 'w3css/w3.css'
 import Todos from './todos';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="w3-teal">
       <Todos />
     </div>
   );
